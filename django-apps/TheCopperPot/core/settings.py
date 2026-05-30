ddffed7049292aa4e549f3858938865fa0bf99ef
Ernest Irungu <ernestirungu55@gmail.com>
2026-05-31 00:00:45 +0300

    Fixed Middleware typo in settings.py

--- django-apps/TheCopperPot/core/settings.py
+++ django-apps/TheCopperPot/core/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -123,4 +123,4 @@ STATICFILES_DIRS = [BASE_DIR / 'restaurant' / 'static']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATIC_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'

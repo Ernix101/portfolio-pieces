aee31cf8dd58cd1242d627f5b0a8cf14a4676f6e
Ernest Irungu <ernestirungu55@gmail.com>
2026-07-06 14:39:59 +0300

    Fixed minor spelling error👀 in settings.py

--- django-apps/TheMedicationTracker/core/settings.py
+++ django-apps/TheMedicationTracker/core/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ['DATABASE_URl'],
+        os.environ['DATABASE_URL'],
         conn_max_age=600,
     )
 }

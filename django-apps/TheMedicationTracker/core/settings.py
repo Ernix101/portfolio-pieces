8f9827b0057af1e0ee87afa7cb4c80bf779b0857
Ernest Irungu <ernestirungu55@gmail.com>
2026-07-06 13:30:36 +0300

    Added some database configurations for railway in settings.py

--- django-apps/TheMedicationTracker/core/settings.py
+++ django-apps/TheMedicationTracker/core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*2j5ye(22si&)#u*ll_91=b_#e=gs015+)54j5r$+j51h9b5up'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 

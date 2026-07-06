78b85b903e460ad558cb6ab67885685736bacfb0
Ernest Irungu <ernestirungu55@gmail.com>
2026-07-06 13:52:09 +0300

    Added load_env(override=False)

--- django-apps/TheMedicationTracker/core/settings.py
+++ django-apps/TheMedicationTracker/core/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 import dj_database_url
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(override=False)
 
 from pathlib import Path
 

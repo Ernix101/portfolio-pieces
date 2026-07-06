71dd9836ae42fe846a133d4d08deb001e051a6df
Ernest Irungu <ernestirungu55@gmail.com>
2026-07-06 13:57:48 +0300

    Reverted from using loadenv()

--- django-apps/TheMedicationTracker/core/settings.py
+++ django-apps/TheMedicationTracker/core/settings.py
@@ -81,11 +81,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=DATABASE_URL,
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
